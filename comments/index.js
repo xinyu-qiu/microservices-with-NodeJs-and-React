1fda16393c6f751facfa8d668e3bffc558a65891
邱欣宇 <qiuxinyu@kyuukinsakais-MacBook-Pro.local>
2022-09-28 16:55:19 -0700

    implement comment

--- comments/index.js
+++ comments/index.js
@@ -1,1 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const { runInNewContext } = require('vm');
+
+const app = express();
+app.use(bodyParser.json());
+
+//record comments
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    //get the id, if there is no such id, get an empty array
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({ id: commentId, content });
+
+    commentsByPostId[req.params.id] = comments;
+
+    //send back entire array of comments
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+});
