f3ec11930389d3415f341bae23339b931406359a
xinyu-qiu <113216096+xinyu-qiu@users.noreply.github.com>
2022-10-21 17:16:07 -0700

    add filter

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -20,6 +20,10 @@ app.post('/events', (req, res) => {
     {
         console.log(err.message);
     });
+    axios.post("http://localhost:4003/events", event).catch((err) => 
+    {
+        console.log(err.message);
+    });
     
     res.send({ status: 'OK' });
 });
--- blog/query/index.js
+++ blog/query/index.js
@@ -22,13 +22,22 @@ app.post('/events', (req, res) => {
     }
 
     if (type === 'CommentCreated') {
-        const { id, content, postId } = data;
+        const { id, content, postId, status } = data;
 
         const post = posts[postId];
-        post.comments.push({ id, content });
+        post.comments.push({ id, content, status });
     } 
 
-    console.log(posts);
+    if (type === 'CommentUpdated') {
+        const {id, content, postId, status } = data;
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+        comment.content = content;
+    }
 
     res.send({});
 });
