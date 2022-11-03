1f49b8c1867162fa6b49da3170a22ad5df7da3ec
xinyu-qiu <113216096+xinyu-qiu@users.noreply.github.com>
2022-11-03 15:08:20 -0700

    section2

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
+
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event).catch((err) => 
     {
@@ -26,6 +30,10 @@ app.post('/events', (req, res) => {
     });
     
     res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => {
