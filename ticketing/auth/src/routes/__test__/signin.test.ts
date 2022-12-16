502a0e346d597851e4fa9f0f725ad0102b67a381
xinyu-qiu <113216096+xinyu-qiu@users.noreply.github.com>
2022-12-16 14:28:52 -0800

    add tests

---
+++ ticketing/auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,48 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('fails when a email not exit', async () => {
+  return request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(400);
+});
+
+it('fails when an incorrect password is supplied', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'ppppppp'
+    })
+    .expect(400);
+});
+
+it('respon with a cookie when given valid credentials', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(200);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
