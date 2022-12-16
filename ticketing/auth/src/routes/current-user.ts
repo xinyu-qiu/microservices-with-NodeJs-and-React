0eedb5020d2ec3a4cee33ea9d130ddc3a2bd11a0
xinyu-qiu <113216096+xinyu-qiu@users.noreply.github.com>
2022-12-16 13:00:12 -0800

    add first test

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import { CurrentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', ( req, res ) => {
-  res.send('Hi there');
+router.get('/api/users/currentuser', CurrentUser, ( req, res ) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,9 +1,50 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { Password } from '../services/password';
+import { User } from '../models/user';
+import { validationRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin  ', ( req, res ) => {
-  res.send('Hi there');
+router.post('/api/users/signin',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('you must apply a password')
+  ], 
+  validationRequest,
+  async (req: Request, res: Response ) => {
+    const { email, password} = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordMatch = await Password.compare(existingUser.password, password);
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const userJwt = jwt.sign({
+      id: existingUser.id,
+      email: existingUser.email
+    }, 
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJwt
+    };
+
+    res.status(200).send(existingUser);
 });
 
 export { router as signinRouter };
