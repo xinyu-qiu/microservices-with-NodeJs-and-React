850a872bafa04872ffba9f92fab80e2f050743cc
xinyu-qiu <113216096+xinyu-qiu@users.noreply.github.com>
2022-12-20 14:41:00 -0800

    add create ticket and tests

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import 'express-async-errors';
 import { json } from "body-parser";
-
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from "@tickets-xinyu/common";
+import { errorHandler, NotFoundError, CurrentUser } from "@tickets-xinyu/common";
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +14,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test'
   })
 );
+app.use(CurrentUser);
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
