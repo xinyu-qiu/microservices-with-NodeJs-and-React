6aa32fd0e0774011dfb412a61fea9c58061d3c70
xinyu-qiu <113216096+xinyu-qiu@users.noreply.github.com>
2022-12-21 14:52:11 -0800

    add tickets tests

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -4,6 +4,9 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, CurrentUser } from "@tickets-xinyu/common";
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set('trust proxy', true);
@@ -16,6 +19,9 @@ app.use(
 );
 app.use(CurrentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
