cd8364bf468739ab1dbbdcf89aaa16a28e715538
xinyu-qiu <113216096+xinyu-qiu@users.noreply.github.com>
2022-12-28 11:40:04 -0800

    add order

---
+++ ticketing/orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+it('makes an order as cancelled', async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app) 
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  const updateOrder = await Order.findById(order.id);
+
+  expect(updateOrder!.status).toEqual(OrderStatus.Cancelled);
+});
